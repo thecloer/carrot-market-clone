3fc757f419ed5a7bf5aa86cd26fb3136d99ea64a
thecloer <chltjrrb97@gmail.com>
2024-10-31 21:35:18 +0900

    add: sms login page

---
+++ src/app/sms/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import { FormInput, FormButton } from '@/components/form';
+
+export default function SMSLoginPage() {
+  return (
+    <main className='flex flex-col gap-y-10 p-6'>
+      <div className='flex flex-col gap-y-2 *:font-medium'>
+        <h1 className='text-2xl'>Log in with SMS.</h1>
+        <h2 className='text-xl'>Verify your phone number.</h2>
+      </div>
+      <form className='flex flex-col gap-y-3'>
+        <FormInput name='phone' key='phone' type='number' placeholder='verification code' required />
+        <FormInput name='verification_code' key='verification code' type='tel' placeholder='Phone number' required />
+        <FormButton type='submit'> Verify</FormButton>
+      </form>
+    </main>
+  );
+}
