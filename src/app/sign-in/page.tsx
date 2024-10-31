59389730afb46a5d829490ebc0b35e7e611829f5
thecloer <chltjrrb97@gmail.com>
2024-10-31 21:33:06 +0900

    add: signin page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+import { FormButton, FormInput, SocialLogin } from '@/components/form';
+
+export default function SignInPage() {
+  return (
+    <main className='flex flex-col gap-y-10 p-6'>
+      <div className='flex flex-col gap-y-2 *:font-medium'>
+        <h1 className='text-2xl'>Welcome!</h1>
+        <h2 className='text-xl'>Connect with your community.</h2>
+      </div>
+      <form className='flex flex-col gap-y-3'>
+        <FormInput name='email' type='email' autoComplete='email' placeholder='Email' required />
+        <FormInput name='password' type='password' autoComplete='current-password' placeholder='Password' required />
+        <FormButton type='submit'>Sign in</FormButton>
+      </form>
+      <SocialLogin />
+    </main>
+  );
+}
