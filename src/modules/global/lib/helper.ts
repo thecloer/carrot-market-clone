b8ea133147a40cf4f2bbf3d90e5651fb92e50262
thecloer <chltjrrb97@gmail.com>
2024-11-02 19:00:48 +0900

    fix: objectMap bug
    used static 'key' insted of dynamic [key]

--- src/modules/global/lib/helper.ts
+++ src/modules/global/lib/helper.ts
@@ -1,5 +1,5 @@
 export const objectMap = <V, R>(obj: Record<string, V>, callback: (value: V, key: string) => R): Record<string, R> =>
   Object.entries(obj).reduce(
-    (result, [key, value]) => ({ ...result, key: callback(value, key) }),
+    (result, [key, value]) => ({ ...result, [key]: callback(value, key) }),
     {} as Record<string, R>
   );
